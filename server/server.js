ff3657307274fc097e432e42e9c0f9cf24f3cc56
prajaktahadole <prajaktahadole@gmail.com>
2022-07-25 20:30:17 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const data = require('./db');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3005;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on ${port}`);
+})
